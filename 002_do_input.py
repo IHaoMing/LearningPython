ff5a9de688863e3541cd4d1cc11bc99b762ecc80
IHaoMing <31131565+IHaoMing@users.noreply.github.com>
2017-08-23 22:33:14 +0800

    输入和输出

---
+++ 002_do_input.py
@@ -0,0 +1,24 @@
+>>> print('hello word')
+hello word
+>>> print('The quick brown fox', 'jumps over', 'the lazy dog')
+The quick brown fox jumps over the lazy dog
+>>> print（300）
+SyntaxError: invalid character in identifier
+>>> print(300)
+300
+>>> print(100 + 200)
+300
+>>> print(' 100 + 200 = ', 100 + 200)
+ 100 + 200 =  300
+>>> name = input()
+Michael
+>>> name
+'Michael'
+>>> print(name)
+Michael
+>>> name = input('please enter your name:')
+please enter your name:Michael
+>>> print('hello,',name)
+hello, Michael
+>>> print('1024 * 768 = ',1024 * 768)
+1024 * 768 =  786432
