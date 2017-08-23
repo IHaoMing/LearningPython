3e5c9d8b39334f2ac500087bc20671bcd2b179b0
IHaoMing <31131565+IHaoMing@users.noreply.github.com>
2017-08-23 22:51:03 +0800

    数据类型和变量

---
+++ 003_variable.py
@@ -0,0 +1,88 @@
+>>> print('I\'m ok.')
+I'm ok.
+>>> print('I\'m learning \n Python')
+I'm learning 
+ Python
+>>> print('\\\n\\')
+\
+\
+>>> print('\\\t\\')
+\	\
+>>> print(r'\\\t\\')
+\\\t\\
+>>> print('''line1
+line2
+line3''')
+line1
+line2
+line3
+>>> True
+True
+>>> False
+False
+>>> 3>2
+True
+>>> 3>5
+False
+>>> True and True
+True
+>>> True and False
+False
+>>> False and False
+False
+>>> 5>3 and 3>1
+True
+>>> True or True
+True
+>>> True or False
+True
+>>> False or False
+False
+>>> 5>3 or 1>9
+True
+>>> not False
+True
+>>> not True
+False
+>>> not 1>3
+True
+>>> age =20
+>>> if age>=18:
+	print('adult')
+	pass
+else:
+	print('teenager')
+	pass
+
+adult
+>>> a = 1
+>>> t_007  = 'T007'
+>>> Answer = True
+>>> PI = 3.14159265359
+>>> 10/3
+3.3333333333333335
+>>> 9/3
+3.0
+>>> 10%3
+1
+>>> n = 123
+>>> f = 456.789
+>>> s1 = 'Hello world'
+>>> s2 = 'Hello, \'Adam\''
+>>> s3 = r'Hello,"Bart"'
+>>> s4 = r'''Hello,
+Lisa!'''
+>>> print(n)
+123
+>>> print(f)
+456.789
+>>> print(s1)
+Hello world
+>>> print(s2)
+Hello, 'Adam'
+>>> print(s3)
+Hello,"Bart"
+>>> print(s4)
+Hello,
+Lisa!
+>>> 
