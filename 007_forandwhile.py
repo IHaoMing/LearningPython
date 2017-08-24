d585b80e906c051c80180deee48766b199d80d37
IHaoMing <31131565+IHaoMing@users.noreply.github.com>
2017-08-24 10:44:11 +0800

    循环

---
+++ 007_forandwhile.py
@@ -0,0 +1,175 @@
+>>> names = ['Michael', 'Bob', 'Tracy']
+>>> for name in names:
+	print(name)
+
+	
+Michael
+Bob
+Tracy
+>>> sum = 0
+>>> for  x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+	sum = sum + x
+print(sum)
+SyntaxError: invalid syntax
+>>> print(sum)
+0
+>>> for x  in  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+SyntaxError: invalid syntax
+>>> for x  in  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+	sum = sum + x
+
+	
+>>> print(sum)
+55
+>>> list(range(5))
+[0, 1, 2, 3, 4]
+>>> sum = 0
+>>> for x in range(101):
+	sum = sum + x
+
+	
+>>> print(sum)
+5050
+>>> sum = 0
+>>> n = 99
+>>> while n>0:
+	sum = sum + n
+	n = n - 2
+
+	
+>>> print(sum)
+2500
+>>> L = ['Bart', 'Lisa', 'Adam']
+>>> for name in L:
+	print('Hello,%s!'%name)
+
+	
+Hello,Bart!
+Hello,Lisa!
+Hello,Adam!
+>>> n = 1
+>>> while n<=100
+SyntaxError: invalid syntax
+>>> while n<=100:
+	print(n)
+	n = n+1
+
+	
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+>>> print('END')
+END
+>>> n = 0
+>>> while n < 10:
+    n = n + 1
+    print(n)
+
+    
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
